869b24653f65d3bcab790bac15a51cd6db9f3b3a
Arhum Javaid <arhum.javaid@gmail.com>
2026-05-31 22:40:02 +0500

    Redis small implementation

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Redis from "ioredis";
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.get("/redis", async (req, res) => {
+    const reply = await redis.ping();
+    res.json({ redis: reply });
+});
+
+
+app.get("/mongo", async (req, res) => {
+    const url = process.env.MONGO_URL || "mongodb://localhost:27017/my_redis_db";
+
+    if(mongoose.connection.readyState === 0) {
+        await mongoose.connect(url)
+    }
+    res.json({ mongo: "connected", "database": mongoose.connection.name });
+}); 
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
